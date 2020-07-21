1b1fb2545bb10fd695dd1a591a25bcabb6813c06
shikhaguptasg94 <shikhaguptasg94@gmail.com>
2020-07-22 00:16:29 +0530

    download pdf changes

--- src/app/scroll-bar/resume-template1/resume-template1.component.ts
+++ src/app/scroll-bar/resume-template1/resume-template1.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Component, OnInit, Output, ElementRef, ViewChild, EventEmitter } from "@angular/core";
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: "app-resume-template1",
@@ -7,13 +9,29 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 })
 export class ResumeTemplate1Component implements OnInit {
   @Output() template1 = new EventEmitter<boolean>();
+  constructor(private hostElement: ElementRef) {
+    console.log(this.hostElement.nativeElement.outerHTML);
+  }
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() { }
 
   closeComponent() {
-    console.log("shikhda gandu");
     this.template1.emit(false);
+  }
+  public convetToPDF() {
+    var data = document.getElementById('page-wrap');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('Resume-File.pdf'); // Generated PDF
+    });
   }
 }
