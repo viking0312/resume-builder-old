a6d51659575c41336d10632779d1eade92053d53
viking0312 <m.maulik96@gmail.com>
2020-07-26 02:38:42 +0530

    added very imporant service. make sure to use it.

--- src/app/scroll-bar/resume-template1/resume-template1.component.ts
+++ src/app/scroll-bar/resume-template1/resume-template1.component.ts
@@ -1,38 +1,58 @@
-import { Component, OnInit, Output, ElementRef, ViewChild, EventEmitter,ViewEncapsulation  } from "@angular/core";
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
+import {
+  Component,
+  OnInit,
+  Output,
+  ElementRef,
+  ViewChild,
+  EventEmitter,
+  ViewEncapsulation,
+} from "@angular/core";
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
+import sampleTemplate from "../../default-data.json";
 
 @Component({
   selector: "app-resume-template1",
   templateUrl: "./resume-template1.component.html",
   styleUrls: ["./resume-template1.component.scss"],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ResumeTemplate1Component implements OnInit {
+  personalInfoObj: any;
+  experienceObj = [];
+  educationObj = [];
+  skillsObj: any;
+  skills: any;
   @Output() template1 = new EventEmitter<boolean>();
   constructor(private hostElement: ElementRef) {
     console.log(this.hostElement.nativeElement.outerHTML);
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.experienceObj = sampleTemplate.template1.experience;
+    this.personalInfoObj = sampleTemplate.template1.personalInfo;
+    this.educationObj = sampleTemplate.template1.education;
+    this.skillsObj = sampleTemplate.template1.skills;
+    this.skills = Object.keys(this.skillsObj);
+  }
 
   closeComponent() {
     this.template1.emit(false);
   }
   public convetToPDF() {
-    var data = document.getElementById('page-wrap');
-    html2canvas(data).then(canvas => {
+    var data = document.getElementById("page-wrap");
+    html2canvas(data).then((canvas) => {
       // Few necessary setting options
       var imgWidth = 208;
       var pageHeight = 295;
-      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
       var heightLeft = imgHeight;
 
-      const contentDataURL = canvas.toDataURL('image/png')
-      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const contentDataURL = canvas.toDataURL("image/png");
+      let pdf = new jspdf("p", "mm", "a4"); // A4 size page of PDF
       var position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-      pdf.save('Resume-File.pdf'); // Generated PDF
+      pdf.addImage(contentDataURL, "PNG", 0, position, imgWidth, imgHeight);
+      pdf.save("Resume-File.pdf"); // Generated PDF
     });
   }
 }
